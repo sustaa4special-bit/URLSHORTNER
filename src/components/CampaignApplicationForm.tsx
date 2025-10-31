419584647b05ccd54d246fd49e8b38810960eeed
[dyad] <git@dyad.sh>
2025-10-31 22:01:38 +0530

    [dyad] Fixing Link import error - wrote 1 file(s)

--- src/components/CampaignApplicationForm.tsx
+++ src/components/CampaignApplicationForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
+import { Link } from "react-router-dom"; // Import Link
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
